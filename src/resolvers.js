f77a50edcfbf2f9f2680b22356b3442fbdcbd0a9
Paulo Linhares <paulitolinhares@gmail.com>
2018-10-24 13:58:19 +0200

    adds all the native fields to pokemon

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,12 +1,34 @@
 const fetch = require('node-fetch');
+const Dataloader = require('dataloader');
+
+const pokemonLoader = new Dataloader(async keys => {
+  return keys.map(async url => {
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    return jsonResponse;
+  })
+})
 
 const resolvers = {
   Query: {
     info: () => 'This is the PokeAPI Graphql wrapper',
     list: async () => {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
-      const parsedResponse = await response.json();
-      return parsedResponse.results;
+      const { results } = await response.json();
+      const olderPokemonUrls = results
+        .slice(0, 151)
+        .map(item => item.url);
+
+      const data = await pokemonLoader.loadMany(olderPokemonUrls);
+      return data;
+    }
+  },
+  Pokemon: {
+    imageFull: ({ id }) => {
+      return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id.toString().padStart(3,'0')}.png`
+    },
+    imageThumb: ({ id }) => {
+      return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id.toString().padStart(3,'0')}.png`
     }
   }
 };
