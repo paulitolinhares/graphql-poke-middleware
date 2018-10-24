960846cd3cfabc3d47f253589cfabedd266736db
Paulo Linhares <paulitolinhares@gmail.com>
2018-10-24 14:45:41 +0200

    finishes the list query

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,13 +1,21 @@
 const fetch = require('node-fetch');
-const Dataloader = require('dataloader');
+const DataLoader = require('dataloader');
 
-const pokemonLoader = new Dataloader(async keys => {
+const pokemonLoader = new DataLoader(async keys => {
   return keys.map(async url => {
     const response = await fetch(url);
     const jsonResponse = await response.json();
     return jsonResponse;
   })
-})
+});
+
+const pokemonMoveLoader = new DataLoader(async keys => {
+  return keys.map(async url => {
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    return jsonResponse;
+  })
+});
 
 const resolvers = {
   Query: {
@@ -16,7 +24,7 @@ const resolvers = {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
       const { results } = await response.json();
       const olderPokemonUrls = results
-        .slice(0, 1) // loads only the real pokemon :P
+        .slice(0, 151) // loads only the real pokemon :P
         .map(item => item.url);
 
       const data = await pokemonLoader.loadMany(olderPokemonUrls);
@@ -30,9 +38,24 @@ const resolvers = {
     imageThumb: ({ id }) => {
       return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id.toString().padStart(3,'0')}.png`
     },
-    types: ({ types }) => {
-      return types.map(({ type: { name } }) => ({ name }));
-    }
+    types: ({ types }) => types.map(({ type: { name } }) => ({ name })),
+    abilities: ({ abilities }) => abilities.map(({ ability: { name } }) => ({ name })),
+    stats: ({ stats }) => stats.map(({ base_stat, stat: { name } }) => ({ name, scale: base_stat })),
+    moves: ({ moves }) => moves.map(({ move }) => move)
+  },
+  PokemonMove: {
+    accuracy: async ({ url }) => {
+      const { accuracy } = await pokemonMoveLoader.load(url);
+      return accuracy;
+    },
+    power: async ({ url }) => {
+      const { power } = await pokemonMoveLoader.load(url);
+      return power;
+    },
+    powerPoints: async ({ url }) => {
+      const { pp } = await pokemonMoveLoader.load(url);
+      return pp;
+    },
   }
 };
 
